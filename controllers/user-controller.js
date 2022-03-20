bd1aa284fec78c3367d26858e4b54e2d27a7b2a5
ryanweiler92 <ryanweiler92@gmail.com>
2022-03-20 08:17:15 -0400

    update user routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -66,7 +66,34 @@ const userController = {
             res.json(dbUserData)
         })
         .catch(err => res.status(400).json(err));
+    },
+
+    addFriend({ params, body }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $push: { friends: body } },
+            { new: true, runValidators: true }
+        )
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id.'});
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
+    },
+    
+    removeFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $pull: { friends: params.friendId} },
+            { new: true }
+        )
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json(err));
     }
 };
+
+
 
 module.exports = userController
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -5,7 +5,10 @@ const {
     getUserById,
     createUser,
     updateUser,
-    deleteUser
+    deleteUser,
+    addFriend,
+    removeFriend
+
 } =  require('../../controllers/user-controller');
 
 router
@@ -18,5 +21,10 @@ router
     .get(getUserById)
     .put(updateUser)
     .delete(deleteUser);
+
+router
+    .route('/:userId/friends/friendId')
+    .post(addFriend)
+    .delete(removeFriend)
 
 module.exports = router;
