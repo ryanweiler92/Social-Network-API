f98aad8ef74c4071ddc6b862fee9c2bd7770e3e1
ryanweiler92 <ryanweiler92@gmail.com>
2022-03-20 08:46:33 -0400

    trouble shoot server.js

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialdb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 mongoose.set('debug', true);
 
